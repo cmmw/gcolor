fd7b336ee6490602368d2797f43e78a90e930d99
Felix Winter <ixos01@gmail.com>
2015-05-05 13:20:31 +0200

    Added benchmark script

---
+++ run_benchmarks.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+program = "./gcolor"
+
+benchmarkdir = "benchmarks/"
+
+filelist = ['david.col', 'huck.col', 'jean.col', 'queen5_5.col', 'queen6_6.col', 'queen7_7.col', 'queen8_12.col', 'queen8_8.col', 'queen9_9.col', 'myciel3.col', 'myciel4.col', 'myciel5.col', 'myciel6.col']
+
+for benchmark in filelist:
+
+    outfile = open(benchmark+".withMAC.txt", "w")
+    try:
+        subprocess.call([program, "--instanceFile", benchmarkdir+benchmark, "--mac", "1"], stdout=outfile)
+    finally:
+        outfile.close()
+    
+    outfile2 = open(benchmark+".txt", "w")
+    try:
+        subprocess.call([program, "--instanceFile", benchmarkdir+benchmark, "--mac", "0"], stdout=outfile2)
+    finally:
+        outfile2.close()
