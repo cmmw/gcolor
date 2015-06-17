fdf98055265932a971eed10e37df4c25d17f1a0a
Felix Winter <ixos01@gmail.com>
2015-06-17 15:30:07 +0200

    Several changes

--- run_benchmarks2.py
+++ run_benchmarks2.py
@@ -2,18 +2,12 @@ import subprocess
 import os
 
 program = "./gcolor"
-timelimit = "300000"
+timelimit = "1200000"
 benchmarkdir = "benchmarks/"
 p= "5"
 tl = "0.4729"
 
-filelist = ['flat1000_60_0.col.b',
-            'fpsol2.i.2.col',
-     'inithx.i.2.col',
-      'le450_25b.col',
-       'mulsol.i.2.col',
-       'miles1000.col', 
-     'queen11_11.col' 
+filelist = ['queen11_11.col' 
 			]
 
 for benchmark in filelist:
