81a07f384c7854bcf7bf7dd90ee35804e6002ba1
Felix Winter <ixos01@gmail.com>
2015-06-17 14:38:30 +0200

    New benchmarks

--- run_benchmarks2.py
+++ run_benchmarks2.py
@@ -2,34 +2,18 @@ import subprocess
 import os
 
 program = "./gcolor"
-timelimit = "3600000"
+timelimit = "300000"
 benchmarkdir = "benchmarks/"
 p= "5"
 tl = "0.4729"
 
-filelist = ['DSJC1000.1.col',
-			'DSJC1000.5.col',
-			'DSJC1000.9.col',
-			'DSJC125.1.col',
-			'DSJC125.5.col',
-			'DSJC125.9.col',
-			'DSJC250.1.col',
-			'DSJC250.5.col',
-			'DSJC250.9.col',
-			'DSJC500.1.col',
-			'DSJC500.5.col',
-			'DSJC500.9.col',
-			'DSJR500.1.col',
-			'DSJR500.1c.col',
-			'DSJR500.5.col',
-			'latin_square_10.col',
-			'school1.col',
-			'school1_nsh.col',
-			'queen10_10.col',
-			'queen12_12.col',
-			'queen14_14.col',
-			'queen15_15.col',
-			'queen16_16.col'
+filelist = ['flat1000_60_0.col.b',
+            'fpsol2.i.2.col',
+     'inithx.i.2.col',
+      'le450_25b.col',
+       'mulsol.i.2.col',
+       'miles1000.col', 
+     'queen11_11.col' 
 			]
 
 for benchmark in filelist:
